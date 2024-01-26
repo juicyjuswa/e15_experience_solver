4d5a788fa4b8c069f0eea3c8c655833aca5364a8
juicyjuswa <braganzajoshua523@gmail.com>
2024-01-27 01:04:58 +0800

    fifteenth exercise

---
+++ e15_experience_solver.py
@@ -0,0 +1,9 @@
+print("EXPONENT CALCULATOR\n")
+base = int(input("Base Number= "))
+exponent = int(input("Exponent= "))
+
+final_value = base ** exponent
+
+print(base, "raises to the power of", exponent, ":", str(final_value):{final_value})
+
+# 2 raises to the power of 5: 32 i.e. (2 *2 * 2 *2 *2 = 32)
