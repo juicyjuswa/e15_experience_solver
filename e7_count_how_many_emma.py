9c65a0d4d137fc65942ab55eb437b78e51f44d8a
juicyjuswa <braganzajoshua523@gmail.com>
2024-01-27 00:21:27 +0800

    seventh exercise

---
+++ e7_count_how_many_emma.py
@@ -0,0 +1,5 @@
+sentence ="Emma is good developer. Emma is a writer"
+sentence_count = sentence.count("Emma")
+
+print("The sentence is: ",sentence)
+print("\nEmma appeared ", sentence_count, "times")
